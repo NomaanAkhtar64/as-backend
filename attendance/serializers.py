b349380ab2282c98dbe77eb68aa89a4af6dcfcd9
Nomaan Akhtar <nomaanakhtar644@gmail.com>
2022-04-06 07:19:04 +0500

    minor serializers editing

--- attendance/serializers.py
+++ attendance/serializers.py
@@ -1,29 +1,8 @@
 from rest_framework import serializers
 from .models import Attendance
-import datetime
 
 
 class AttendanceSerializer(serializers.ModelSerializer):
-    employee_status = serializers.SerializerMethodField()
-
-    def get_employee_status(self, obj):
-        if obj.joining_date == None:
-            return "Active"
-        if (
-            obj.joining_date
-            > datetime.datetime.now(tz=datetime.timezone.tzname("Asia/Karachi")).date()
-        ):
-            return "Active"
-        return "Ex-Employee"
-
     class Meta:
         model = Attendance
-        fields = (
-            "id",
-            "first_name",
-            "last_name",
-            "wage_per_hour",
-            "joining_date",
-            "leaving_date",
-            "employee_status",
-        )
+        fields = "__all__"
